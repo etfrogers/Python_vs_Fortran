ae76ce15136eb18e224dd5d740e11baadcbb6494
Ed Rogers <etfrogers@hotmail.com>
2018-01-26 16:28:02 +0000

    Naive matlab version - faster than optimised

--- magic_index_pot.m
+++ magic_index_pot.m
@@ -7,15 +7,7 @@ N = size(r, 1);
 soft2 = soft^2;
 x = (1:N);
 [xg,yg] = meshgrid(x,x);
-inds1 = tril(xg, -1);
-inds1 = inds1(:);
-inds1(inds1==0) = [];
-
-inds2 = tril(yg, -1);
-inds2 = inds2(:);
-inds2(inds2==0) = [];
-
 % indices = np.broadcast_to(range(N),(N,N)).T;
 dr = sqrt(sum((r(xg(:),:) - r(yg(:),:)).^2, 2) + soft2);
-pot = sum(1./dr,2);
+pot = sum(tril(1./dr,-1));
 toc
